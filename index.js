c5ab4aa5546b966e524d1e53e066122b654e09a1
Michael <oliengo.michael@gmail.com>
2024-04-01 16:34:52 +0300

    complete Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    //returns the number of blocks given a value
+    if (someValue<42){
+      return(42-someValue);
+    }
+    else {
+  
+    return(someValue-42);
+  }
+  }
+  
+
+  function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+  }
+
+  function distanceTravelledInFeet(x,y){
+    if (y>x){
+      return((y-x)*264);
+    }
+    else
+      return((x-y)*264);
+  }
+  
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -62,3 +62,19 @@ describe('index.js', function() {
     });
   });
 });
+
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
+  }
+  if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
+  }
+  else if(distance>=2000&& distance<=2500){
+    return 25;
+  }
+  else if (distance > 2500){
+    return "cannot travel that far";
+  }
+}
